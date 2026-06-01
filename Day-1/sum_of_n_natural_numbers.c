419933aad00dde67d511841eac1651679de2566a
arpittomar1403-coder <arpittomar1403@gmail.com>
2026-06-01 15:16:20 +0530

    Update sum_of_n_natural_numbers.c

--- Day-1/sum_of_n_natural_numbers.c
+++ Day-1/sum_of_n_natural_numbers.c
@@ -1,11 +1,15 @@
 #include<stdio.h>
 int main(){
+// n is the user input, sum stores the total , i is for the loop
   int n, sum = 0, i;
+  // for value of the number
   printf("Enter n:");
   scanf("%d",&n);
+  // loop from 1 to n , to keep adding the numbers
   for (i=1;i<=n;i++){
-    sum += i;
+    sum += i; // for adding current number to the total
   }
+  //output the final result
 printf("sum =%d",sum);
 return 0;
 }

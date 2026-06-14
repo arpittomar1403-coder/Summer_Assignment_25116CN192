b250462677cfe3648de997e5cfba7891c9b494de
arpittomar1403-coder <arpittomar1403@gmail.com>
2026-06-14 21:38:21 +0530

    Day-14 uploaded.

---
+++ Day-14/frequency_of_an_element.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+int arr[100], n, element, i, count = 0;
+printf("Enter number of elements: ");
+scanf("%d", &n);
+printf("Enter %d elements:\n", n);
+for(i = 0; i < n; i++) {
+scanf("%d", &arr[i]);
+}
+printf("Enter element whose frequency is to be found: ");
+scanf("%d", &element);
+for(i = 0; i < n; i++) {
+if(arr[i] == element) {
+count++;
+ }
+}
+printf("Frequency of %d = %d", element, count);
+return 0;
+}
